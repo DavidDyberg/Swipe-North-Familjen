1c1bb80ec4e2fe436473c0c2f6c71cc7ffc0350b
unknown <david.dyberg@edu.cmeducations.se>
2023-03-22 22:59:34 +0100

    fixade pilarna till startsidan

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 import styles from './styles/startsida.module.css'
+import Link from 'next/link'
 import photo from '../public/Swipe-North-logga-vit.png'
 import photo2 from '../public/linkedin.png'
 import photo3 from '../public/google.png'
 import photo4 from '../public/facebook.webp'
+import photo5 from '../public/pilar-image.png'
 
+const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Startsida() {
@@ -19,18 +19,18 @@ export default function Startsida() {
 				<div className={styles['logga-container']} >
 					<Image className={styles['swipe-north-logga']} src={photo}></Image>
 				</div>
-				<div className={styles['arrows-symbols-container']}>
-					<p className={styles['arrow-up-symbol']}>&#8686;</p>
-					<p className={styles['arrow-right-symbol']}>&#8686;</p>
-					<p className={styles['arrow-down-symbol']}>&#8686;</p>
-					<p className={styles['arrow-left-symbol']}>&#8686;</p>
+				
+				<Link href='/swipe-north'>
+					<button className={styles.swipebutton}>BÖRJA SWIPEA</button>
+				</Link>
+				
+				<div className={styles['arrows-container']}>
+					<Image className={styles.arrowimage} src={photo5}></Image>
 				</div>
-				
-				
-
+					
 				<div className={styles['button-container']}>
-					<button className={styles.button} >SKAPA KONTO</button>
 					<button className={styles.button} >LOGGA IN</button>
+					<button className={styles.button} >SKAPA KONTO</button>
 				</div>
 
 				<div className={styles['icon-container']}>
