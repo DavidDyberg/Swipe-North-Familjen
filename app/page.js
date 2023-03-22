e1bec660913c02ef85687354228943717fabfcc5
unknown <david.dyberg@edu.cmeducations.se>
2023-03-22 11:05:43 +0100

    flyttade startsida mapp

--- app/page.js
+++ app/page.js
@@ -3,10 +3,38 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+import styles from './styles/startsida.module.css'
+import photo from '../public/Swipe-North-logga-vit.png'
+import photo2 from '../public/linkedin.png'
+import photo3 from '../public/google.png'
+import photo4 from '../public/facebook.webp'
+
+
+
+export default function Startsida() {
 	return (
-		<main>
-			<h1>Home</h1>
-		</main>
+		
+			<main className={styles.main}>
+				
+				<div className={styles['logga-container']} >
+					<Image className={styles['swipe-north-logga']} src={photo}></Image>
+				</div>
+				<p className={styles['arrow-up-symbol']}>&#8686;</p>
+				<h1 className={styles.title} >VÄLKOMMET TILL SWIPE NORTH</h1>
+				<p className={styles.text}>SWIPEA UPPÅT FÖR ATT HITTA JUST DITT DRÖMJOBB</p>
+
+				<div className={styles['button-container']}>
+					<button className={styles.button} >SKAPA KONTO</button>
+					<button className={styles.button} >LOGGA IN</button>
+				</div>
+
+				<div className={styles['icon-container']}>
+					<Image className={styles.icon} src={photo2}></Image>
+					<Image className={styles.icon} src={photo3}></Image>
+					<Image className={styles.icon} src={photo4}></Image>
+				</div>
+				
+			</main>
+		
 	)
 }
