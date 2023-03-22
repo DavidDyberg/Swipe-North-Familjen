5cd0c72bbb016c13702a48f163adf4dbe71e1d1c
Paola <paola.vidal@edu.cmeducations.se>
2023-03-22 10:36:18 +0100

    layout

--- app/(pages)/layout.js
+++ app/(pages)/layout.js
@@ -1,34 +1,13 @@
-'use client'
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
-
+import Navbar from '../components/hamburger'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<nav>
-					<Link className="links" href="#">
-						Home
-					</Link>
-					<Link className="links" href="/swipe-north">
-						Jobb api
-					</Link>
-					<Link className="links" href="/skelleftea">
-						Skellefteå
-					</Link>
-					<Link className="links" href="/profile">
-						Profile
-					</Link>
-					<Link className="links" href="/matched-jobs">
-						Matched jobs
-					</Link>
-					<Link className="links" href="/login">
-						Log in
-					</Link>
-				</nav>
+				<Navbar />
 
 				<main>{children}</main>
 			</body>
--- app/layout.js
+++ app/layout.js
@@ -15,26 +15,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-			<nav>
-					<Link className="links" href="#">
-						Home
-					</Link>
-					<Link className="links" href="/swipe-north">
-						Jobb api
-					</Link>
-					<Link className="links" href="/skelleftea">
-						Skellefteå
-					</Link>
-					<Link className="links" href="/profile">
-						Profile
-					</Link>
-					<Link className="links" href="/matched-jobs">
-						Matched jobs
-					</Link>
-					<Link className="links" href="/login">
-						Log in
-					</Link>
-				</nav>
 
 				<main>{children}</main>
 			</body>
