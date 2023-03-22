bde7d679dd79a1c844ba477991058941b85e610c
unknown <david.dyberg@edu.cmeducations.se>
2023-03-22 15:59:17 +0100

    fixade bakgrunden i startsidan

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={inter.className}>
 
-				<main>{children}</main>
+				{children}
 			</body>
 		</html>
 	)
