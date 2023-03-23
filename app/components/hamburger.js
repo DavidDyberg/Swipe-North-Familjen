40eaa35870eedc5331ef380c6d11082f252dc4ea
Paola <paola.vidal@edu.cmeducations.se>
2023-03-23 14:31:01 +0100

    function closing menu

--- app/components/hamburger.js
+++ app/components/hamburger.js
@@ -6,56 +6,63 @@ import Logga from '../../public/Swipe-North-logga-svart.png'
 import Ske from '../../public/skelleftea.png'
 import Image from 'next/image'
 
+// export default function Navbar() {
+// 	const [isOpen, setIsOpen] = useState(false)
+// 	const openMenu = () => setIsOpen(!isOpen)
+
 export default function Navbar() {
 	const [isOpen, setIsOpen] = useState(false)
-	const openMenu = () => setIsOpen(!isOpen)
+  
+	const toggleMenu = () => setIsOpen(!isOpen)
+	const closeMenu = () => setIsOpen(false)
 
 	return (
 		<div className="container">
 			<nav className="navbar">
 				<Image src={Ske} className="Ske"></Image>
 				<Image src={Logga} className="Logga"></Image>
-				<ul
-					className={
-						isOpen === false
-							? 'navMenu'
-							: 'navMenu' + ' ' + 'active'
-					}
-				>
+				<ul  className={`navMenu ${isOpen ? 'active' : ''}`}>
+				{/* // 	className={
+				// 		isOpen === false
+				// 			? 'navMenu'
+				// 			: 'navMenu' + ' ' + 'active'
+				// 	}
+				// > */}
 					<li className="navItem">
 						<Link href="../swipe-north">
-							<span className="navLink">Hitta jobb</span>
+							<span onClick={closeMenu} className="navLink">Hitta jobb</span>
 						</Link>
 					</li>
 					<li className="navItem">
 						<Link href="../profile">
-							<span className="navLink">Profil</span>
+							<span onClick={closeMenu} className="navLink">Profil</span>
 						</Link>
 					</li>
 					<li className="navItem">
 						<Link href="../matched-jobs">
-							<span className="navLink">Sparade jobb</span>
+							<span onClick={closeMenu} className="navLink">Sparade jobb</span>
 						</Link>
 					</li>
 					<li className="navItem">
 						<Link href="../skelleftea">
-							<span className="navLink">Skellefteå</span>
+							<span onClick={closeMenu} className="navLink">Skellefteå</span>
 						</Link>
 					</li>
 					<li className="navItem">
 						<Link href="../login">
-							<span className="navLink">Logga in</span>
+							<span onClick={closeMenu} className="navLink">Logga in</span>
 						</Link>
 					</li>
 				</ul>
 				<button
-					className={
-						isOpen === false
-							? 'hamburger'
-							: 'hamburger' + ' ' + 'active'
-					}
-					onClick={openMenu}
-				>
+				className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+				{/* // 	className={
+				// 		isOpen === false
+				// 			? 'hamburger'
+				// 			: 'hamburger' + ' ' + 'active'
+				// 	}
+				// 	onClick={openMenu}
+				// > */}
 					<span className="navbarButton"></span>
 					<span className="navbarButton"></span>
 					<span className="navbarButton"></span>
