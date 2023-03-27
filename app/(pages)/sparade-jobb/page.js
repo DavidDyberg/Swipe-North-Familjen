1e2451dacdef0258e9acbad17804158e2a6a68e1
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-03-27 13:54:16 +0200

    Sparade jobb.

--- app/(pages)/sparade-jobb/page.js
+++ app/(pages)/sparade-jobb/page.js
@@ -2,28 +2,35 @@
 import { useState, useEffect } from 'react'
 
 function SavedJobs() {
-  const [savedIds, setSavedIds] = useState([])
+  let [savedIds, setSavedIds] = useState([]);
 
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('savedIds'))
-    setSavedIds(data)
-  }, [])
+    const data = JSON.parse(localStorage.getItem('savedIds'));
+    setSavedIds(data);
+  }, []);
+
+  // check if savedIds is null and insert a placeholder job
+  if (savedIds === null) {
+    savedIds = [{ headline: "Inga sparade annonser" }];
+  }
 
   return (
-    <div>
+    <div className='text-card shadow saved-jobs'>
       <h1>Sparade jobb</h1>
-	  <br />
-	  <br />
+      <br />
+      <br />
       {savedIds.map((job) => (
         <div key={job.id}>
           <h2>{job.headline}</h2>
           <p>{job.employerName}</p>
 		  <br />
+      <hr />
 	  	<br />
         </div>
       ))}
     </div>
-  )
+  );
 }
+
 
 export default SavedJobs
