e2835c7411267b4162c64b397a55728442a8943b
Paola <paola.vidal@edu.cmeducations.se>
2023-03-28 08:42:54 +0200

    favicon

--- app/layout.js
+++ app/layout.js
@@ -8,12 +8,16 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
 	title: 'Swipe North',
 	description: 'Hitta ditt nya jobb i Skellefteå',
+	icons : {
+		icon: './favicon-16x16.png',
+
+	}
 }
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-					<Head>
+					{/* <Head>
 						<Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                         <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -21,7 +25,7 @@ export default function RootLayout({ children }) {
 <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
 <meta name="msapplication-TileColor" content="#da532c" />
 <meta name="theme-color" content="#ffffff"></meta>
-		</Head>
+		</Head> */}
 
 			<body className={inter.className}>{children}</body>
 		</html>
