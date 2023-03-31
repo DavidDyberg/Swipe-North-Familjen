fa537be9acc054a1cce0d0b3172ace4aefdd6942
Paola <paola.vidal@edu.cmeducations.se>
2023-03-31 10:36:53 +0200

    meta

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,17 @@ export const metadata = {
 	icons: {
 		icon: './favicon-32x32.png',
 	},	
-	other: {
-		appleMobileWebAppCapable: "yes",
-	},
+	appleWebApp: {
+		title: 'Apple Web App',
+		statusBarStyle: 'black-translucent',
+		startupImage: [
+		  '/assets/startup/apple-touch-startup-image-768x1004.png',
+		  {
+			url: '/assets/startup/apple-touch-startup-image-1536x2008.png',
+			media: '(device-width: 768px) and (device-height: 1024px)',
+		  },
+		  ]
+		}
 };
 
 export default function RootLayout({ children }) {
