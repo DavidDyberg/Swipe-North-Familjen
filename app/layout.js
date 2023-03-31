ec2697e18bb85f0017addf2bb0dca486364f9a2c
Paola <paola.vidal@edu.cmeducations.se>
2023-03-31 08:49:38 +0200

    meta

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import './styles/globals.css'
 import { Inter } from 'next/font/google'
 // import Link from 'next/link'
-// import Head from 'next/head'
+import Head from 'next/head'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+<Head>
+<meta name="apple-mobile-web-app-capable" content="yes" />
+</Head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
